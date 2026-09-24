2168f07972d495c13ee970dea8f3a20fa9a31cd7
bansikumar12 <bansipatel251204@gmail.com>
2026-09-24 12:15:58 +0530

    Refactor TaskList component to use destructured props

--- src/components/Auth/TaskList/TaskList.jsx
+++ src/components/Auth/TaskList/TaskList.jsx
@@ -4,32 +4,26 @@ import NewTask from './NewTask'
 import CompleteTask from './CompleteTask'
 import FailedTask from './FailedTask'
 
-const TaskList = (props) => {
+const TaskList = ({ data }) => {
+    return (
+        <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16'>
+            {data.tasks.map((elem, idx) => {
+                if (elem.active) {
+                    return <AcceptTask key={idx} data={elem} />
+                }
+                if (elem.newTask) {
+                    return <NewTask key={idx} data={elem} />
+                }
+                if (elem.completed) {
+                    return <CompleteTask key={idx} data={elem} />
+                }
+                if (elem.failed) {
+                    return <FailedTask key={idx} data={elem} />
+                }
 
-  
+            })}
+        </div>
+    )
+}
 
-  return (
-    <div
-      id="tasklist"
-      className="w-full flex overflow-x-auto items-center justify-start gap-5 flex-nowrap h-[55%] m-10"
-    >
-      {props.data.tasks.map((elem, idx) => {
-        if (elem.active) {
-          return <AcceptTask key={idx} data={elem} />;
-        }
-        if (elem.newTask) {
-          return <NewTask key={idx} data={elem} />;
-        }
-        if (elem.completed) {
-          return <CompleteTask key={idx} data={elem} />;
-        }
-        if (elem.failed) {
-          return <FailedTask key={idx} data={elem} />;
-        }
-       
-      })}
-    </div>
-  );
-};
-
-export default TaskList;
+export default TaskList
