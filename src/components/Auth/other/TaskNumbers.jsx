76a02e71893d850d724150b9a7fe066d690252f4
bansikumar12 <bansipatel251204@gmail.com>
2026-09-24 11:49:12 +0530

    Refactor TaskNumbers component to TaskListNumbers

--- src/components/Auth/other/TaskNumbers.jsx
+++ src/components/Auth/other/TaskNumbers.jsx
@@ -1,44 +1,27 @@
 import React from 'react'
 
-const TaskNumbers = (props) => {
-
-   
-
-  
-
+const TaskListNumbers = ({data}) => {
   return (
-    <div className='flex justify-between mt-10 gap-5 flex-wrap'>
-      
-      <div className='w-[45%] p-4 bg-red-400 text-white  rounded-md'>
-        <p className='text-3xl font-bold'>
-          {props.data.taskCounts.newTask}
-        </p>
-        <h2 className='text-xl font-bold'>New Tasks</h2>
-      </div>
-
-      <div className='w-[45%] p-4 bg-blue-400 text-white rounded-md'>
-        <p className='text-3xl font-bold'>
-          {props.data.taskCounts.completed}
-        </p>
-        <h2 className='text-xl font-bold'>Completed</h2>
-      </div>
-
-      <div className='w-[45%] p-4 bg-green-400 text-white rounded-md'>
-        <p className='text-3xl font-bold'>
-          {props.data.taskCounts.active}
-        </p>
-        <h2 className='text-xl font-bold'>Active</h2>
-      </div>
-
-      <div className='w-[45%] p-4 bg-yellow-400 text-white rounded-md'>
-        <p className='text-3xl font-bold'>
-          {props.data.taskCounts.failed}
-        </p>
-        <h2 className='text-xl font-bold'>Failed</h2>
-      </div>
-
+    <div className='flex mt-10 justify-between gap-5 screen'>
+        
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-blue-400'>
+            <h2 className='text-3xl font-bold'>{data.taskCounts.newTask}</h2>
+            <h3 className='text-xl mt-0.5 font-medium'>New Task</h3>
+        </div>
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-green-400'>
+            <h2 className='text-3xl font-bold'>{data.taskCounts.completed}</h2>
+            <h3 className='text-xl mt-0.5 font-medium'>Completed Task</h3>
+        </div>
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-yellow-400 '>
+            <h2 className='text-3xl text-black font-bold'>{data.taskCounts.active}</h2>
+            <h3 className='text-xl mt-0.5 text-black font-medium'>Accepted Task</h3>
+        </div>
+        <div className='rounded-xl w-[45%] py-6 px-9 bg-red-400'>
+            <h2 className='text-3xl font-bold'>{data.taskCounts.failed}</h2>
+            <h3 className='text-xl mt-0.5 font-medium'>Failed Task</h3>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default TaskNumbers;
+export default TaskListNumbers
