2fe3067aa21907c1a73c94e6ff85865ec1e7b836
bansikumar12 <bansipatel251204@gmail.com>
2026-09-24 12:13:40 +0530

    Update CompleteTask.jsx

--- src/components/Auth/TaskList/CompleteTask.jsx
+++ src/components/Auth/TaskList/CompleteTask.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const CompleteTask = (props) => {
+const CompleteTask = ({data}) => {
   return (
-     <div className=" bg-blue-400 flex-shrink-0 p-5 font-bold text-white rounded-2xl text-2xl h-full w-[300px]">
-          <div className='flex justify-between'>
-            <h3 className='bg-red-600 px-3 text-sm text-white text-bold py-1 rounded'>{props.data.category}</h3>
-            <h4 className='text-lg'>{props.data.date} </h4>
-          </div>
-            <h2 className='text-xl mt-2 font-bold'>{props.data.title}</h2>
-            <p className='mt-5 text-sm'>{props.data.description}</p>
-            <div className='  mt-5'>
-            <button className="bg-green-500 text-white text-lg px-2 py-1 rounded">Complete</button>
-          </div>  
-      </div>
+    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+                <h4 className='text-sm'>{data.taskDate}</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+            <p className='text-sm mt-2'>
+                {data.taskDescription}
+            </p>
+            <div className='mt-6'>
+                <button className='w-full bg-green-600 rounded font-medium py-1 px-2 text-xs'>Complete</button>
+            </div>
+        </div>
   )
 }
 
