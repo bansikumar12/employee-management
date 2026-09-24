aa769d1fb07d6a8405dffcdf7afde5d9cc2d02c3
bansikumar12 <bansipatel251204@gmail.com>
2026-09-24 11:51:26 +0530

    Refactor EmployeeDashboard component imports and usage

--- src/components/Auth/Dashboard/EmployeeDashboard.jsx
+++ src/components/Auth/Dashboard/EmployeeDashboard.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Headers from '../other/Header'
-import TaskNumbers from '../other/TaskNumbers'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
 import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = (props) => {
-   console.log("Employee Data:", props.data);
-  
+
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
-       <Headers  changeUser={props.changeUser} data={props.data} />
-       <TaskNumbers data={props.data} />
-       <TaskList data={props.data}  />
+        
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
     </div>
   )
 }
